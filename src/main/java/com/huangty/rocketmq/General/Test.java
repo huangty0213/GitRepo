3b0f0ec4155eed15699609b42525d87243669ae7
huangty <huangty@jiagouyun.com>
2020-08-13 21:09:44 +0800

    modify main()

--- src/main/java/com/huangty/rocketmq/General/Test.java
+++ src/main/java/com/huangty/rocketmq/General/Test.java
@@ -1,7 +1,16 @@
 package com.huangty.rocketmq.General;
 
+import java.util.Scanner;
+
 public class Test {
     public static void main(String[] args) {
-        System.out.println("hello.");
+        System.out.println("welcome "+getName());
+    }
+
+    public static String getName(){
+        System.out.println("Please input your name:");
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        return s;
     }
 }
